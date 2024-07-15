89ae8e289581dacd1aa3aa52991acde128485e6c
Javed <javedofficialme@gmail.com>
2024-07-15 21:38:22 +0530

    complete day 2

---
+++ Day_02/src/index.ts
@@ -0,0 +1,58 @@
+//--Activity 1
+
+let a = 2;
+let b = 3;
+// Task 1
+console.log('Addition', a + b);
+
+// Task 2
+console.log('Substraction', a - b);
+
+// Task 3
+console.log('Multiply', a * b);
+
+// Task 4
+console.log('Divide', a / b);
+
+// Task 5
+console.log('Remainder', a % b);
+
+//--Activity 2
+
+// Task 6
+a += 5;
+console.log(a);
+
+// Task 7
+b -= 1;
+console.log(b);
+
+//--Activity 3
+
+// Task 8
+console.log(a < b);
+console.log(a > b);
+
+// Task 9
+console.log(a >= b);
+console.log(a <= b);
+
+// Task 10
+console.log(a == b);
+console.log(a === b);
+
+//--Activity 4
+
+// Task 11
+console.log(a !== b && a <= b);
+// Task 12
+console.log(a == b || a > b);
+// Task 13
+console.log(!a);
+
+//--Activity 5
+
+// Task 14
+console.log(a >= 0 ? 'positive' : 'negative');
+
+//--Activity 6
