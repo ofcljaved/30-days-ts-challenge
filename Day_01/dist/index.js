d2dfb3dee92666efc8dbd831fb7d207bbcee3b0e
Javed <javedofficialme@gmail.com>
2024-07-15 20:31:37 +0530

    complete Day 01

---
+++ Day_01/dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+//--Activity 1
+// Task 1
+var num = 23;
+console.log('varibale using var keyword:', num);
+// Task 2
+let str = 'bruhh';
+console.log('varibale using let keyword:', str);
+//--Activity 2
+// Task 3
+const is_male = true;
+console.log('varibale using const keyword:', is_male);
+//--Activity 3
+// Task 4
+const number = 12;
+const string = 'geez';
+const bool = false;
+const obj = { age: 99 };
+const arr = [1, 2, 3];
+console.log('number', typeof number, 'string', typeof string, 'bool', typeof bool, 'obj', typeof obj, 'arr', typeof arr);
+//--Activity 4
+// Task 5
+let reassign = 23;
+console.log('First value', reassign);
+reassign = 'hehe';
+console.log('After reassigning', reassign);
+//--Activity 5
+// Task 6
+const newReassign = 32;
+console.log('First value', newReassign);
+//@ts-ignore
+newReassign = 'string';
+console.log('Second value', newReassign);
+//--Activity 6
---
+++ Day_01/src/index.ts
@@ -0,0 +1,55 @@
+//--Activity 1
+
+// Task 1
+var num = 23;
+console.log('varibale using var keyword:', num);
+
+// Task 2
+let str = 'bruhh';
+console.log('varibale using let keyword:', str);
+
+//--Activity 2
+
+// Task 3
+const is_male = true;
+console.log('varibale using const keyword:', is_male);
+
+//--Activity 3
+
+// Task 4
+const number = 12;
+const string = 'geez';
+const bool = false;
+const obj = { age: 99 };
+const arr = [1, 2, 3];
+console.log(
+  'number type',
+  typeof number,
+  'string type',
+  typeof string,
+  'bool type',
+  typeof bool,
+  'obj type',
+  typeof obj,
+  'arr type',
+  typeof arr
+);
+
+//--Activity 4
+
+// Task 5
+let reassign: any = 23;
+console.log('First value', reassign);
+reassign = 'hehe';
+console.log('After reassigning', reassign);
+
+//--Activity 5
+
+// Task 6
+const newReassign: any = 32;
+console.log('First value', newReassign);
+
+// newReassign = 'string'; //Cannot assign to 'newReassign' because it is a constant.
+console.log('Second value', newReassign);
+
+//--Activity 6
